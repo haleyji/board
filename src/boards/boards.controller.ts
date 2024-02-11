2e370b437dcc7b347875884671de019e119076be
haleyji <hyeyounghaleyji@gmail.com>
2024-02-11 22:57:38 +0900

    CRUD Board

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,12 +1,21 @@
-import { Controller, Get } from '@nestjs/common';
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { BoardsService } from './boards.service';
+import { Board } from './boards.model';
 
 @Controller('boards')
 export class BoardsController {
   constructor(private boardService: BoardsService) {}
 
   @Get('/')
-  getAllBoards() {
+  getAllBoards(): Board[] {
     return this.boardService.getAllBoards();
+  }
+  @Post()
+  createBoard(
+    @Body('title') title: string,
+    @Body('description') description: string,
+  ): Board {
+    return this.boardService.createBoard(title, description);
   }
 }
